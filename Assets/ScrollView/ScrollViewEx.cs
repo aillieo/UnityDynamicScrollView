2e9d02ac88c910f8c1c7e249b153813012df6583
aillieo <aillieo@163.com>
2020-09-25 21:23:42 +0800

    add ScrollViewEx

---
+++ Assets/ScrollView/ScrollViewEx.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using SObject = System.Object;
+
+namespace AillieoUtils
+{
+    [RequireComponent(typeof(RectTransform))]
+    [DisallowMultipleComponent]
+    public class ScrollViewEx : ScrollView
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            onValueChanged.AddListener(OnValueChanged);
+        }
+
+        [SerializeField]
+        private int m_pageSize = 50;
+
+        public int pageSize => m_pageSize;
+
+        private int startOffset = 500;
+
+        private Func<int> realItemCountFunc;
+
+        public override void SetUpdateFunc(Action<int, RectTransform> func)
+        {
+            if(func != null)
+            {
+                var f = func;
+                func = (index, rect) => {
+                    f(index + startOffset, rect);
+                };
+            }
+            base.SetUpdateFunc(func);
+        }
+
+        public override void SetItemSizeFunc(Func<int, Vector2> func)
+        {
+            if (func != null)
+            {
+                var f = func;
+                func = (index) => {
+                    return f(index + startOffset);
+                };
+            }
+            base.SetItemSizeFunc(func);
+        }
+
+        public override void SetItemCountFunc(Func<int> func)
+        {
+            realItemCountFunc = func;
+            if(func != null)
+            {
+                var f = func;
+                func = () => Mathf.Min(f(), pageSize);
+            }
+            base.SetItemCountFunc(func);
+        }
+
+        private void OnValueChanged(Vector2 position)
+        {
+            int toShow;
+            int critical;
+            bool downward;
+            int pin;
+            if (((int)layoutType & flagScrollDirection) == 1)
+            {
+                // 垂直滚动 只计算y向
+                if (velocity.y > 0)
+                {
+                    // 向上
+                    toShow = criticalItemIndex[CriticalItemType.DownToShow];
+                    critical = pageSize - 1;
+                    if (toShow < critical)
+                    {
+                        return;
+                    }
+                    pin = critical - 1;
+                    downward = false;
+                }
+                else
+                {
+                    // 向下
+                    toShow = criticalItemIndex[CriticalItemType.UpToShow];
+                    critical = 0;
+                    if(toShow > critical)
+                    {
+                        return;
+                    }
+                    pin = critical + 1;
+                    downward = true;
+                }
+            }
+            else // = 0
+            {
+                // 水平滚动 只计算x向
+                if (velocity.x > 0)
+                {
+                    // 向右
+                    toShow = criticalItemIndex[CriticalItemType.UpToShow];
+                    critical = 0;
+                    if (toShow > critical)
+                    {
+                        return;
+                    }
+                    pin = critical + 1;
+                    downward = true;
+                }
+                else
+                {
+                    // 向左
+                    toShow = criticalItemIndex[CriticalItemType.DownToShow];
+                    critical = pageSize - 1;
+                    if (toShow < critical)
+                    {
+                        return;
+                    }
+                    pin = critical - 1;
+                    downward = false;
+                }
+            }
+
+            // 该翻页了 翻半页吧
+            int old = startOffset;
+            if (downward)
+            {
+                startOffset -= pageSize / 2;
+            }
+            else
+            {
+                startOffset += pageSize / 2;
+            }
+
+            int realDataCount = 0;
+            if (realItemCountFunc != null)
+            {
+                realDataCount = realItemCountFunc();
+            }
+            startOffset = Mathf.Clamp(startOffset, 0, Mathf.Max(realDataCount - pageSize, 0));
+
+            if (old != startOffset)
+            {
+                if(downward)
+                {
+
+                }
+
+
+                // 记录 原先的速度
+                Vector2 oldVelocity = velocity;
+                // 计算 pin元素的世界坐标
+                Rect rect = GetItemLocalRect(pin);
+                Vector2 oldWorld = content.TransformPoint(rect.position);
+                UpdateData(true);
+                // 根据 pin元素的世界坐标 计算出content的position
+                int pin2 = pin + old - startOffset;
+                Rect rect2 = GetItemLocalRect(pin2);
+                Vector2 newWorld = content.TransformPoint(rect2.position);
+                Vector2 deltaWorld = newWorld - oldWorld;
+                Vector2 deltaLocal = content.InverseTransformVector(deltaWorld);
+                // Debug.LogError($"critical={critical} toShow={toShow} pin={pin} pin2={pin2} pinpos={rect.position} pin2pos={rect2.position} pinworld={oldWorld} pin2world={newWorld} deltaLocal = {deltaLocal}");
+                SetContentAnchoredPosition(content.anchoredPosition - deltaLocal);
+                UpdateData(true);
+                UpdateData(false);
+                // Debug.LogError($"critical={critical} toShow={toShow} pin={pin} pin2={pin2} pinpos={rect.position} pin2pos={GetItemLocalRect(pin2).position} pinworld={oldWorld} pin2world={content.TransformPoint(GetItemLocalRect(pin2).position)} ===");
+                // 取回速度
+                velocity = oldVelocity;
+            }
+
+        }
+    }
+}
---
+++ Assets/Test/TestLargeAmount.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using AillieoUtils;
+using UnityEngine.UI;
+using SObject = System.Object;
+using System.Text;
+using System.Diagnostics;
+
+public class TestLargeAmount : MonoBehaviour {
+
+    List<DefaultScrollItemData> testData = new List<DefaultScrollItemData>();
+
+    void updateFunc(int index, RectTransform item)
+    {
+        DefaultScrollItemData data = testData[index];
+        item.gameObject.SetActive(true);
+        item.transform.Find("Text").GetComponent<Text>().text = string.Format("{0}_{1}", data.name, index);
+    }
+
+    Vector2 itemSizeFunc(int index)
+    {
+        // TimeConsumingFunc();
+        // test =====================================================================================
+
+
+        DefaultScrollItemData sd = testData[index];
+        if (sd.name == "XXL")
+            return new Vector2(300, 150);
+        else if (sd.name == "XL")
+            return new Vector2(300, 125);
+        else if (sd.name == "L")
+            return new Vector2(300, 100);
+        else if (sd.name == "M")
+            return new Vector2(300, 75);
+        else // "S"
+            return new Vector2(300, 50);
+    }
+
+    int itemCountFunc()
+    {
+        return testData.Count;
+    }
+
+    public ScrollViewEx scrollView;
+
+    void Start () {
+
+        scrollView.SetUpdateFunc(updateFunc);
+        scrollView.SetItemSizeFunc(itemSizeFunc);
+        scrollView.SetItemCountFunc(itemCountFunc);
+
+        int dataCount = 0;
+        do
+        {
+            AddRandomData();
+        }
+        while (++dataCount < 50000);
+
+        scrollView.UpdateData(false);
+    }
+
+    static string GetRandomSizeString()
+    {
+        float f = UnityEngine.Random.value;
+        if(f > 0.8)
+        {
+            return "XXL";
+        }
+        else if(f > 0.6)
+        {
+            return "XL";
+        }
+        else if (f > 0.4)
+        {
+            return "L";
+        }
+        else if (f > 0.2)
+        {
+            return "M";
+        }
+        else
+        {
+            return "S";
+        }
+    }
+
+    private void TimeConsumingFunc()
+    {
+        string s = string.Empty;
+        int a = UnityEngine.Random.Range(0, 10000);
+        int b = UnityEngine.Random.Range(0, 10000);
+
+        for (int i = a; i < a + b; ++ i)
+        {
+            s += i;
+        }
+    }
+
+    public void AddRandomData()
+    {
+        DefaultScrollItemData newData = new DefaultScrollItemData() { name = GetRandomSizeString()};
+        testData.Insert(UnityEngine.Random.Range(0,testData.Count), newData);
+        scrollView.UpdateData(false);
+    }
+
+    public void RemoveRandomData()
+    {
+        if(testData.Count == 0)
+        {
+            return;
+        }
+        int index = UnityEngine.Random.Range(0, testData.Count);
+        testData.RemoveAt(index);
+        scrollView.UpdateData(false);
+    }
+
+    public void ScrollToRandom()
+    {
+        int index = UnityEngine.Random.Range(0, testData.Count);
+
+        // test =====================================================================================
+        // always last
+        index = testData.Count - 1;
+
+        scrollView.ScrollTo(index);
+        scrollView.UpdateData(false);
+    }
+}
